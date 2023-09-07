56f7d37abe41ef5e92ac0582cf10a7a1e46ab4aa
crahteam <sanascoot7@gmail.com>
2023-09-07 14:10:29 +0200

    +

--- target/package/hedel-rs-0.1.1/target/debug/deps/unicode_ident-27c541bdd3194d90.d
+++
@@ -1,8 +0,0 @@
-/home/tux/Desktop/hedel-rs/target/package/hedel-rs-0.1.1/target/debug/deps/unicode_ident-27c541bdd3194d90.rmeta: /home/tux/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/lib.rs /home/tux/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/tables.rs
-
-/home/tux/Desktop/hedel-rs/target/package/hedel-rs-0.1.1/target/debug/deps/libunicode_ident-27c541bdd3194d90.rlib: /home/tux/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/lib.rs /home/tux/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/tables.rs
-
-/home/tux/Desktop/hedel-rs/target/package/hedel-rs-0.1.1/target/debug/deps/unicode_ident-27c541bdd3194d90.d: /home/tux/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/lib.rs /home/tux/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/tables.rs
-
-/home/tux/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/lib.rs:
-/home/tux/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/tables.rs:
